0f55b34b02bbee978a60f56761aef0061c59273c
Zach1204 <113856048+Zach1204@users.noreply.github.com>
2024-02-29 17:19:32 -0500

    Add files via upload

--- lab 6/py_code/main.py
+++ lab 6/py_code/main.py
@@ -17,13 +17,9 @@ client: Client = Client(intents=intents)
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message: print('empty') 
 
-#if you put "?" before a pokemon name it will DM results
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
-
     try:
         response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        await message.author.send(response)
     except Exception as e:
         print(e)
 
@@ -36,13 +32,9 @@ async def on_ready() -> None:
 @client.event
 async def on_message(message: Message) -> None:
     if message.author == client.user: return
-
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
+    response_embed = get_response(message.content)
+    await message.channel.send(embed=response_embed)
+    
 
 #runs bot
 def main() -> None:
--- lab 6/py_code/responses.py
+++ lab 6/py_code/responses.py
@@ -1,4 +1,5 @@
 import requests
+from discord import Embed
 
 #converts pokemon name to lower
 def get_response(user_input: str) -> str: 
@@ -8,20 +9,32 @@ def get_response(user_input: str) -> str:
     response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{lowered}')
     
     #ported data from api into stats
-    name = response.json()['forms'][0]['name']
-    abilities = response.json()['abilities']
+    pokemon_data = response.json()
+    name = pokemon_data['forms'][0]['name'].capitalize()
+    abilities = pokemon_data['abilities']
     abilities_info = []
+    base_stats = pokemon_data['stats']
+    sprite_url = pokemon_data['sprites']['front_default']
     
     #takes in abilites and descriptions
     for ability in abilities:
-            ability_name = ability['ability']['name']
+            ability_name = ability['ability']['name'].capitalize()
             ability_url = ability['ability']['url']
             ability_description = requests.get(ability_url).json()['effect_entries'][1]['short_effect']
             abilities_info.append(f"__{ability_name}__: {ability_description}")
 
 
-    #return
-    if lowered == "":
-        return 'Well well well'
-    elif f'{name}' in lowered:
-        return f'{name} and abilities:\n{"\n".join(abilities_info)}'
+    #results!
+    info = Embed(title=name)
+    
+    info.set_image(url=sprite_url)
+    
+    info.add_field(name="Abilities:", value="\n".join(abilities_info), inline=True)
+    
+    for stat in base_stats:
+        stat_name = stat['stat']['name'].capitalize()  # Capitalize stat name
+        stat_value = stat['base_stat']
+        info.add_field(name=stat_name, value=stat_value, inline=False)
+    
+    
+    return info 
